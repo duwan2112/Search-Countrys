265553fde9a90a3b0f22d9ebffa0260603d6f201
Duwan Peña <duwanp2112@gmail.com>
2020-07-06 16:21:01 -0430

    Updates

---
+++ precache-manifest.21b7ed9d8e4a8ffd0c35b7e0e9e9fd3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db73d7dee674423ccb3d059c1540ea62",
+    "url": "/Search-Countrys/index.html"
+  },
+  {
+    "revision": "ac115a9898f1237831c6",
+    "url": "/Search-Countrys/static/css/main.42935188.chunk.css"
+  },
+  {
+    "revision": "7d9af2b3472cc18fdc5f",
+    "url": "/Search-Countrys/static/js/2.8b1ed745.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Search-Countrys/static/js/2.8b1ed745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac115a9898f1237831c6",
+    "url": "/Search-Countrys/static/js/main.bb2ee938.chunk.js"
+  },
+  {
+    "revision": "afd7937f1c430a1b4052",
+    "url": "/Search-Countrys/static/js/runtime-main.9e568142.js"
+  }
+]);
